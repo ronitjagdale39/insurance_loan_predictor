4aa93adb67264431affd9c7e1e31dfa1c5a7feb8
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2025-12-19 21:32:35 +0530

    Created the endpoint for prediction of our trained model.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from schema.schema import UserInfo
+from model.predict import predicted_output
 app=FastAPI()
 @app.get('/')
 def home():
@@ -11,3 +13,20 @@ def status():
     return {
         'status':'OK'
     }
+@app.post('/prediction')    
+def prediction(user:UserInfo)    ->str:
+    input_info={
+        'bmi':user.bmi,
+        'age_group':user.age_group,
+        'city_tier':user.city_tier,
+        'lifestyle':user.lifestyle,
+        'income_lpa':user.income_lpa,
+        'occupation':user.occupation
+    }
+    try:
+        prediction_out=predicted_output(input_info)
+        return JSONResponse(status_code=200,content={
+            'response':prediction_out
+        })
+    except Exception as e:
+        return JSONResponse(status_code=500,content=str(e))    
