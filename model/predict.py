4754efef517efb78d0be6e53f88d6b613a0f32b6
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-01-02 14:31:03 +0530

    Chnges in predict.py

--- model/predict.py
+++ model/predict.py
@@ -1,8 +1,12 @@
 import pickle
+import os
 import pandas as pd
 # loading the model
-filename='model/model.pkl'
-with open(filename,'rb') as f:
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_PATH = os.path.join(BASE_DIR, "model", "model.pkl")
+
+with open(MODEL_PATH,'rb') as f:
     model=pickle.load(f)
 def predicted_output(input_info:dict) :
     df=pd.DataFrame([input_info])
