8e1560b153fa34eb9ad3bf9db65750b2ac66d478
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-01-02 14:07:57 +0530

    added some chnges in app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from schema.schema import UserInfo
 from model.predict import predicted_output
-app=FastAPI()
+app=FastAPI(title="Insurance premium predictor",version="1.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 @app.get('/')
 def home():
     return {
@@ -27,4 +35,4 @@ def prediction(user:UserInfo) :
         prediction_out=predicted_output(input_info)
         return JSONResponse(status_code=200,content=prediction_out)
     except Exception as e:
-        return JSONResponse(status_code=500,content=str(e))    
+        return JSONResponse(status_code=500,content=str(e))   
