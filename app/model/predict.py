991a68c6310fe60f24b71c21a038e4d5c254cc16
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-06-16 12:19:03 +0530

    Backend problem resolved.

--- app/model/predict.py
+++ app/model/predict.py
@@ -3,8 +3,12 @@ import os
 import pandas as pd
 # loading the model
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_PATH = os.path.join(BASE_DIR, "model", "model.pkl")
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
+
+print("MODEL_PATH =", MODEL_PATH)
+print("EXISTS =", os.path.exists(MODEL_PATH))
 
 with open(MODEL_PATH,'rb') as f:
     model=pickle.load(f)
