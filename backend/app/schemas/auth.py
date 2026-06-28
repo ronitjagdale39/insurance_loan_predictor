b82fe67cc6521971ff03cfecc5c21cb6f33e4b46
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-06-28 21:13:59 +0530

    Added auth to system .

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr,Field
+class SignupRequest(BaseModel):
+    name: str
+    email: EmailStr
+    phone_no: str
+    password: str=Field(
+        min_length=8,
+        max_length=64
+    )
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
