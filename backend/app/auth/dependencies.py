fcb5249d53ffed73a3c2f7b9760bdff737318410
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-07-01 17:02:01 +0530

    Added and configured the auth settings .

---
+++ backend/app/auth/dependencies.py
@@ -0,0 +1,32 @@
+from fastapi import Depends,HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from jose import jwt,JWTError
+from app.models.user import User
+from app.db.database import get_db
+from app.core.security import SECRET_KEY,ALGORITHM
+Oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token:str=Depends(Oauth2_scheme),db:Session=Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401,detail="token invalid")
+        user=db.query(User).filter(User.id==int(user_id)).first()
+        if not user:
+            raise HTTPException(status_code=401,detail="Invalid Authetication Credentials ....")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+def role_required(allowed_roles: list):
+    def wrapper(current_user:User=Depends(get_current_user)):
+        if current_user.role not in allowed_roles:
+            raise HTTPException(
+                status_code=403,
+                detail="Access denied"
+            )
+        return current_user
+    return wrapper
