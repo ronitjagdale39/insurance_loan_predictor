57616e22740c084ac28bceac118e6981b37fce14
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-07-07 13:22:23 +0530

    fix(config):added new attributes.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,14 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+    VERIFICATION_TOKEN_EXPIRE_MINUTES: int
+    RESET_PASSWORD_TOKEN_EXPIRE_MINUTES:int
+    EMAIL_HOST: str
+    EMAIL_PORT: int
+    EMAIL_USER: str
+    EMAIL_PASSWORD: str
+    EMAIL_FROM: str
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
