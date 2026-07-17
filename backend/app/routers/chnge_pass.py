e228b8b12955c0f567fd40786bbc264645fd8daf
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2026-07-17 19:23:59 +0530

    feat : added audit log for chnge password .

--- backend/app/routers/chnge_pass.py
+++ backend/app/routers/chnge_pass.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 
 from app.models.user import User
+from fastapi import Request
+from app.services.audit_log import AuditService
 
 from app.core.security import verify_password, hashed_password
 
@@ -13,6 +15,7 @@ from app.auth.dependencies import get_current_user
 router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/change-password")
 def change_password(
+    request:Request,
     old_password: str,
     new_password: str,
     db: Session = Depends(get_db),
@@ -21,10 +24,27 @@ def change_password(
 
 
     if not verify_password(old_password, current_user.hashed_password):
+        AuditService.log(
+            db=db,
+            event="CHANGE_PASSWORD",
+    level="WARNING",
+    user_id=current_user.id,
+    email=current_user.email,
+    endpoint=request.url.path,
+    method=request.method,
+    status_code=401,
+    ip_address=request.client.host if request.client else None,
+    message="Invalid old password",
+    payload={
+        "reason":"Invalid or old password is incorrect"
+    }
+        )
+
         raise HTTPException(
             status_code=401,
             detail="Invalid old password"
         )
+
 
 
     current_user.hashed_password = hashed_password(new_password)
@@ -34,7 +54,21 @@ def change_password(
 
     db.commit()
     db.refresh(current_user)
-
+    AuditService.log(
+            db=db,
+            event="PASSWORD_CHANGED",
+    level="SUCCESS",
+    user_id=current_user.id,
+    email=current_user.email,
+    endpoint=request.url.path,
+    method=request.method,
+    status_code=200,
+    ip_address=request.client.host if request.client else None,
+    message="Password changed successfully",
+    payload={
+        "Password Chnged":True
+    }
+        )
     return {
         "message": "Password changed successfully",
         "status": "success"
